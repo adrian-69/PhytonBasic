850ff1e1795ce69d233ad32c948bf0555c3b96b4
adrian-69 <aperez20995@iesjoanramis.org>
2024-12-02 14:03:09 +0100

    T10ex20

---
+++ T10ex20.py
@@ -0,0 +1,17 @@
+def invertir(s):
+    return s[::-1]
+
+def es_palindorm(s):
+    if s == invertir(s):
+        return True
+    else:
+        return False
+
+s = input("Introdueixi una cadena: ")
+a = invertir(s)
+if es_palindrom(s):
+    print("La frase/paraula {} és igual a {} i, per tant, és palíndrom".format(s, a))
+else:
+    print("La frase/paraula {} no és igual a {} i, per tant, no és palindrom".format(s, a))
+
+    
