21dcb374104df7989860f98f0950996121c4f189
adrian-69 <aperez20995@iesjoanramis.org>
2024-12-03 09:09:42 +0100

    exercici24

---
+++ T10ex24.py
@@ -0,0 +1,19 @@
+def llegir_llista():
+    l=[]
+    a="a"
+    while a!=".":
+        a=input("Introdueix un número: ")
+        if a!=".":
+            l.append(int(a))
+    return l
+def crear_punts(l):
+    s="."
+    for e in l:
+        print("{} \n".format(s*e))
+        s="."
+#Programa principal
+a = llegir_llista()
+b=llegir_llista()
+crear_punts(a)
+crear_punts(b)
+crear_punts(a)
