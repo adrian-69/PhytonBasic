2f596c1b7187b2fb36be096cbde6f4b4d8554797
adrian-69 <aperez20995@iesjoanramis.org>
2024-12-04 12:35:00 +0100

    Delete T10ex7.py

--- T10ex7.py
+++
@@ -1,9 +0,0 @@
-def es_vocal(caracter):
-    return caracter.lower() in 'aeiou'
-
-# Proves amb diferents exemples
-print(es_vocal('a'))  
-print(es_vocal('E'))  
-print(es_vocal('t'))  
-print(es_vocal('u')) 
-print(es_vocal('x')) 
